4a7a8c08be298856cbf9a84cb6f834523609ce6c
shubhamdhungana <49474810+shubhamdhungana@users.noreply.github.com>
2019-05-12 23:02:52 -0700

    Update style.js

--- style.js
+++ style.js
@@ -9,21 +9,21 @@ if (loveScore>70) {
 
 }
 
-if (loveScore>50 && loveScore <=70) {
-  alert("You Love Score " + loveScore + "%. " + yourName+",Thik cha thik cha , lageraho munna bhai with " + loverName );
+else if (loveScore>50 && loveScore <=70) {
+  alert("You Love Score " + loveScore + "%. " + yourName+", Thik cha thik cha , lageraho munna bhai with " + loverName );
 
 }
-if (loveScore>30 && loveScore <=50) {
-  alert("You Love Score " + loveScore + "%. " + yourName+",Halka Tero connection kam bho " + loverName + " sanga" );
+else if (loveScore>30 && loveScore <=50) {
+  alert("You Love Score " + loveScore + "%. " + yourName+", Halka Tero connection kam bho " + loverName + " sanga" );
 
 }
-if (loveScore>10 && loveScore <=30) {
+else if (loveScore>10 && loveScore <=30) {
 
-  alert("You Love Score " + loveScore + "%. " + yourName+",Kati kam connection " + loverName + " sanga" );
+  alert("You Love Score " + loveScore + "%. " + yourName+", Tero Kati kam connection " + loverName + " sanga" );
 
 
 }
-(loveScore>1 && loveScore <=10) {
+else if(loveScore>1 && loveScore <=10) {
   alert("You Love Score " + loveScore + "%. " + yourName+",Ta ghar jaa. Ta baata hudaina " + loverName + " sanga" );
 
 
