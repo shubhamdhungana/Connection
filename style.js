abc47b594921e0b152f276417832a261f9461409
Shubham Dhungana <49474810+shubhamdhungana@users.noreply.github.com>
2020-12-05 12:52:43 +0545

    Update style.js

--- style.js
+++ style.js
@@ -5,26 +5,26 @@ var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore)+1;
 
 if (loveScore>70) {
-  alert("You Love Score " + loveScore + "%. " + yourName+",You are made for each other i.e. " + loverName );
+  alert("You Love Score " + loveScore + "%. " + yourName+",Kya baat hain! Kya baat hain!! Bacchey kaa naam sochlo " + loverName + "aur tumhaara");
 
 }
 
 else if (loveScore>50 && loveScore <=70) {
-  alert("You Love Score " + loveScore + "%. " + yourName+", Thik cha thik cha , lageraho munna bhai with " + loverName );
+  alert("You Love Score " + loveScore + "%. " + yourName+",Wow, Thik cha thik cha , lageraho munna bhai with " + loverName );
 
 }
 else if (loveScore>30 && loveScore <=50) {
-  alert("You Love Score " + loveScore + "%. " + yourName+", Halka Tero connection kam bho " + loverName + " sanga" );
+  alert("You Love Score " + loveScore + "%. " + yourName+",Halka Timro connection kam bho, feri try gara la. LOL" + loverName + " sanga" );
 
 }
 else if (loveScore>10 && loveScore <=30) {
 
-  alert("You Love Score " + loveScore + "%. " + yourName+", Tero Kati kam connection " + loverName + " sanga" );
+  alert("You Love Score " + loveScore + "%. " + yourName+", Haha! Bohot Kam Hain. LOL! LOL!! " + loverName + " sanga" );
 
 
 }
 else if(loveScore>1 && loveScore <=10) {
-  alert("You Love Score " + loveScore + "%. " + yourName+",Ta ghar jaa. Ta baata hudaina " + loverName + " sanga" );
+  alert("You Love Score " + loveScore + "%. " + yourName+",Tumsey naa hopaayegaa! Kuch nahi hogaa tumhara " + loverName + " sanga" );
 
 
 }
